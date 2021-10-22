657d8e5084ae261fa4cc22156a972f669d2ace27
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-10-22 14:41:37 +0600

    add navbar

--- src/components/Homepage/Dashboard/Dashboard.js
+++ src/components/Homepage/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from '../Navbar/Navbar';
 import UpcomingLaunches from '../UpcomingLaunches/UpcomingLaunches';
 
 const Dashboard = () => {
@@ -7,28 +8,24 @@ const Dashboard = () => {
         fetch('https://api.spacexdata.com/v4/launches')
             .then(res => res.json())
             .then(data => {
-                // console.log(data);
                 setLaunches(data);
             })
     }, [])
     return (
-        <section>
-            <div class="relative items-center w-full px-5 py-12 mx-auto  md:px-12 lg:px-24 max-w-7xl">
-                <div class="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
+        <div>
+            <Navbar/>
+            <section>
+                <div class="relative items-center w-full px-5 py-12 mx-auto  md:px-12 lg:px-24 max-w-7xl">
+                    <div class="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
 
-                    {
-                        launches.map((launch) => <UpcomingLaunches launch={launch}></UpcomingLaunches>)
-                    }
+                        {
+                            launches.map((launch) => <UpcomingLaunches launch={launch}></UpcomingLaunches>)
+                        }
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     );
 };
 
 export default Dashboard;
-{/* <div className="grid grid-cols-3 gap-4">
-
-    {
-        launches.map((launch) => <UpcomingLaunches launch={launch}></UpcomingLaunches>)
-    }
-</div> */}
